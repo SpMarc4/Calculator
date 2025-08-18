c01a8b8570d3027e7a9ce205db9c7e7be0227c58
Marc San <sanchezpe.marc@gmail.com>
2025-08-18 23:00:01 +0200

    Crear funciones operaciones
    Se crean las funciones encargadas de realizar las operaciones.

---
+++ calculator.js
@@ -0,0 +1,27 @@
+function add(a,b) {
+    return a + b
+};
+
+function subtract(a,b) {
+    return a - b
+};
+
+function multiply(a,b) {
+    return a * b
+};
+
+function divide(a,b) {
+    return a / b
+};
+
+add(3,4)
+add("3","4")
+
+subtract(3,4)
+subtract("3","4")
+
+multiply(3,4)
+multiply("3","4")
+
+divide(3,4)
+divide("3","4")
