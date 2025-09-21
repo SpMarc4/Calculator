a322377f12506cbbcb80ae69a5b53f7abdd014f9
Marc San <sanchezpe.marc@gmail.com>
2025-09-21 15:05:20 +0200

    Creation of the operand of the calculator
    Creation of the operand logic and solve issues with the display.

--- calculator.js
+++ calculator.js
@@ -1,17 +1,21 @@
 function add(a,b) {
-    return a + b
+    let result = Number(a) + Number(b)
+    return String(result)
 };
 
 function subtract(a,b) {
-    return a - b
+    let result = Number(a) - Number(b)
+    return String(result)
 };
 
 function multiply(a,b) {
-    return a * b
+    let result = Number(a) * Number(b)
+    return String(result)
 };
 
 function divide(a,b) {
-    return a / b
+    let result = Number(a) / Number(b)
+    return String(result)
 };
 
 let firstNum = 0;
@@ -20,19 +24,19 @@ let operator = '+';
 
 function operate (a,b, operator) {
     if (operator == '+') {
-        add(a,b);
+        return add(a,b);
     }
 
     else if (operator == '-') {
-        subtract(a,b);
+        return subtract(a,b);
     }
 
     else if (operator == '*') {
-        multiply(a,b);
+        return multiply(a,b);
     }
 
     else if (operator == '/') {
-        divide(a,b);
+        return divide(a,b);
     }
 
     else {
@@ -42,21 +46,132 @@ function operate (a,b, operator) {
     }
 };
 
-function display_num (num){
-    // Select the element "display" of the calculator
-    let display = document.querySelector(".display");
-    // Extract and save the value of the number clicked
-    numVal = num.target.textContent;
-    console.log(numVal)
-    // Add the content of the number clicked in the "display element"
-    display.textContent += numVal;
 
-};
+
+// Select numbers from the DOM
 
 let numbers = document.querySelectorAll(".num");
-numbers.forEach( function (elem) {
-    if ((elem.textContent != ' ')) 
+
+
+let operands = document.querySelectorAll(".op");
+
+// Select display from the DOM
+
+let display = document.querySelector(".display");
+
+// Select special characters from the DOM
+
+let extraButtons = document.querySelector(".eb")
+
+// Initialize variable value 1 aka accumulator
+
+let value1 = '';
+
+// Initialize variable value 2 aka actualValue
+
+let value2 = '';
+
+// Initialize variable operand
+
+let operand = '';
+
+// Initialize the display
+
+display.textContent = value1+operand+value2;
+
+
+// Creation of a function to update the display
+
+function updateDisplay() {
+    display.textContent = value1+operand+value2;
+};
+
+
+// If there's not operand we accumulate the clicked value in value 1
+// else in value 2
+
+function valueAccumulator(value) {
+    if (operand == '') {
+        value1 += value
+        console.log(`Value1 : ${value1}`)
+    }
+    else {
+        value2 += value
+        console.log(`Value2 : ${value2}`)
+
+    }
+    updateDisplay()
+
+}
+
+// We create an event to change value 1 or value 2
+
+numbers.forEach( function (num) {
+    if ((num.textContent != ' ')) 
         {
-            console.log(elem.textContent)
-            elem.addEventListener("click",display_num)}
+        num.addEventListener("click", 
+            function () {
+                valueAccumulator(num.textContent);}
+            );
+        }
     })
+
+
+// Operate if the operand is not null
+
+function operandOperate (opClicked) {
+    // If the operand is != ''  operate value 1 and value 2
+    if (operand != '') {
+    
+        // Update value 1 to the result of the operation
+        value1 = operate(value1,value2,operand);
+        
+        // Update value 2 to ''
+        value2 = '';
+    }
+}
+
+// Update the operand
+
+function operandUpdate (opClicked) {
+    
+    // Update the operation
+    operand = opClicked
+    
+    // Display of the values
+    updateDisplay()
+
+}
+
+// We create an event listener to update the operand when it's clicked
+
+operands.forEach( function (op) {
+        op.addEventListener("click", 
+            function () {
+                operandOperate(op.textContent);
+                operandUpdate(op.textContent);}
+            );
+    })
+
+
+
+// The undo function will extract the last character of the display.
+// But it's important to update the values and operand. So it's
+// necesary to know which is the last non Null value. We are going
+// to extract characters from the end to the start.
+// value2 -> operand -> value1
+
+
+// The function clear will be responsible of reset all the values
+
+function clear() {
+    // Reset of the values and operand
+    value1 = '';
+    value2 = '';
+    operand = '';
+    updateDisplay()
+
+}
+
+
+
